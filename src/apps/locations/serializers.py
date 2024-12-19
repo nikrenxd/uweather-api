27ba7271a7bb53cea1c4dadd94afcb08d553df00
nikrenxd <nikrenc1101@gmail.com>
2024-12-19 18:58:36 +0200

    serializer for location temparature

--- src/apps/locations/serializers.py
+++ src/apps/locations/serializers.py
@@ -20,3 +20,20 @@ class LocationSerializer(serializers.ModelSerializer):
         }
 
         return super().to_internal_value(return_data)
+
+
+class LocationDataSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    current_temperature = serializers.DecimalField(max_digits=5, decimal_places=2)
+    min_temperature = serializers.DecimalField(max_digits=5, decimal_places=2)
+    max_temperature = serializers.DecimalField(max_digits=5, decimal_places=2)
+
+    def to_internal_value(self, data):
+        return_data = {
+            "name": data["name"],
+            "current_temperature": data["main"]["temp"],
+            "min_temperature": data["main"]["temp_min"],
+            "max_temperature": data["main"]["temp_max"],
+        }
+
+        return super().to_internal_value(return_data)
