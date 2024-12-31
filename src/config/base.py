2472c2865d51eeb9f0857b4473e8d9e8f50b32ca
nikrenxd <nikrenc1101@gmail.com>
2024-12-31 22:51:07 +0200

    config changes

--- src/config/base.py
+++ src/config/base.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings
 
 class Config(BaseSettings):
     ALLOWED_HOSTS: str
+    CSRF_TRUSTED_ORIGINS: str
     DEBUG: int = Field(default=0)
     DJANGO_SECRET: str
     EXTERNAL_API_KEY: str
